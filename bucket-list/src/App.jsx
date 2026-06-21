13a2ebfb2c054652dc9aa36552a7f0f457dc5e5b
PlayZhed <barnio25@googlemail.com>
2026-06-21 19:55:13 +0200

    added local storage for list items and minor optical changes

--- bucket-list/src/App.jsx
+++ bucket-list/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [bucketList, setBucketList] = useState([
-    "learn spanish",
-    "learn to fly",
-    "parachute",
-  ]);
+  const [bucketList, setBucketList] = useState(() => {
+      const saved = localStorage.getItem("bucketList");
+      return saved ? JSON.parse(saved) : ["example"];
+  });
 
 const [input, setInput] = useState("");
+
+useEffect(() => {
+    localStorage.setItem("bucketList", JSON.stringify(bucketList));
+}, [bucketList]);
 
 const removeItem = (index) => {
     setBucketList(bucketList.filter((_,I) => I !== index));
@@ -27,9 +30,9 @@ function addItem() {
       <h1>Meine Bucket List</h1>
       <input value={input} onChange={(e) =>setInput(e.target.value)}
              placeholder="my next todo"  />
-      <button className="addItem-button" onClick={addItem}>add</button>
+      <button className="addItem-button" onClick={addItem}>+</button>
       <div className="list"> {bucketList.map((item, index) => ( <div className="list-item" key={index}> {item}
-      <button className="removeItem-button" onClick={() => removeItem(index)}>remove</button>
+      <button className="removeItem-button" onClick={() => removeItem(index)}>x</button>
       </div>))}
       </div>
      </>
